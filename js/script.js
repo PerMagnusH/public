7fc6316165bde2144d1e0b7151886f98eec944a9
PerMagnusH <per.magnus.rickard@gmail.com>
2018-03-09 10:25:53 +0100

    Create js/script.js

--- js/script.js
+++ js/script.js
@@ -16,6 +16,9 @@
             var track = findGetParameter("track");
             var srcAddr = "https://w.soundcloud.com/player/?url=https://api.soundcloud.com/tracks/" + track + "&amp;color=%23ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=false";
             iframe.attr("src", srcAddr); 
-            $("#postYourAdd").attr("disabled", "disabled"); 
+            if (track !== '410118795')
+            {
+                $("#postYourAdd").attr("disabled", "disabled");
+            }
         }
     
